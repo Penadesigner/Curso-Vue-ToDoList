660511a1cb1378adc3face2754001f4f507aeaa6
Pena <gui.domenes@gmail.com>
2023-01-05 15:29:56 -0300

    novo

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,62 @@
 import { createStore } from 'vuex'
+import http from '@/http'
 
 export default createStore({
   state: {
+    todos: []
   },
   getters: {
   },
   mutations: {
+    storeTodos(state, payload){
+      state.todos = payload
+    },
+
+    storeTodo(state, payload){
+      const index = state.todos.findIndex(t => t.id == payload.id)
+      if(index >= 0){
+        state.todos.splice(index, 1, payload) 
+      } else {
+        state.todos.push(payload)   // se usar o unshift ao inves do push, o valor entra na primeira posicao
+      }
+    },
+
+    deleteTodo(state, id){
+      const index = state.todos.findIndex(t => t.id == id)
+      if(index >= 0){
+        state.todos.splice(index, 1) 
+      }
+    },
+
   },
   actions: {
+  // o return faz com que apos o then ser finalizado, 
+  // a resposta vai ser enviada para o componente que solicitou o dispatch.
+    getTodos({commit}){
+      this.loading = true
+      return http.get("todos").then(res => {  
+        commit('storeTodos', res.data)
+      })
+    },
+
+    addTodo({commit}, data){
+      return http.post("todos", data).then(res => {
+        commit('storeTodo', res.data)
+      })
+    },
+
+    updateTodo({commit}, {id, data}){
+      return http.put(`todos/${id}`, data).then(res => {
+        commit('storeTodo', res.data)
+      })
+    },
+    
+    deleteTodo({commit}, id){
+      return http.delete(`todos/${id}`).then(() => {
+        commit('deleteTodo', id)
+      })
+    }
+  
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <!-- Content -->
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+         
+            <TodoSpinner v-if="loading"/>
+          <template v-else>
+            <TodoFormAdd/>
+
+            <TodoItems v-if="$store.state.todos.length"/>
+
+            <TodoEmpyt v-else/>
+          </template>
+        </div>
+    </div>
+    <!--/ Content -->
   </div>
 </template>
 
 <script>
+import TodoSpinner from '@/components/TodoSpinner.vue'
+import TodoFormAdd from '@/components/TodoFormAdd.vue'
+import TodoItems from '@/components/TodoItems.vue'
+import TodoEmpyt from '@/components/TodoEmpyt.vue'
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  components: { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpyt },
   name: 'HomeView',
-  components: {
-    HelloWorld
+
+  setup(){
+    // DATA
+    const store = useStore()
+    const loading = ref(false)
+
+    // CREATED
+    loading.value = true
+    store.dispatch('getTodos').finally(() => {
+      loading.value = false
+    })
+
+    // O QUE VAI PARA O TEMPLATE
+    return {
+      loading
+    }
+    
   }
+
 }
 </script>
